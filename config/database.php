c166d441421133dd4f73c116bd43c343c2b8fe72
PatriciaVallejos <patriciarivera@emision.co>
2022-10-13 16:03:05 -0500

    Config database

--- config/database.php
+++ config/database.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
+$DATABASE_URL = parse_url('La url copiada aqui');
 
 return [
 
@@ -14,8 +15,9 @@ return [
     | you may use many connections at once using the Database library.
     |
     */
+    
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -24,7 +26,7 @@ return [
     |
     | Here are each of the database connections setup for your application.
     | Of course, examples of configuring each database platform that is
-    | supported by Laravel is shown below to make development simple.
+    | supported by Laravel is shown below to make development simple.favicon
     |
     |
     | All database work in Laravel is done through the PHP PDO facilities
@@ -32,7 +34,7 @@ return [
     | choice installed on your machine before you begin development.
     |
     */
-
+    
     'connections' => [
 
         'sqlite' => [
@@ -68,7 +70,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
+            'database' => ltrim($DATABASE_URL["path"], "/"),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
